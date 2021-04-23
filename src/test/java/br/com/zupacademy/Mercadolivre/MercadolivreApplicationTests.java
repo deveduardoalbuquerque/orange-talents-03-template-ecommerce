53e7cedc70e20eb86df13c41f1a03cd9afd82062
DevEduardoAlbuquerque <luiz.barbosa@zup.com.br>
2021-04-23 11:28:52 -0300

    setup inicial

---
+++ src/test/java/br/com/zupacademy/Mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.Mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
